ab31b218a9ac406aa292749256ebc6211bd4ca25
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2018-07-28 10:39:43 +0200

    Setup front-end proxy

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "http://localhost:3000/api";
 
 const API = {
   FetchChallanges: () => fetch(`${BASE_URL}/challanges`).then(r => r.json()),
