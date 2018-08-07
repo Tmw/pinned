8a92c2bdae6cbeea07d9e5354fb91c75d924860e
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2018-08-07 20:25:37 +0200

    Drop unused code

--- backend/server/handlers.go
+++ backend/server/handlers.go
@@ -11,18 +11,3 @@ func (s *Server) getChallangesHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(challanges)
 }
-
-func (s *Server) notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	errorResponse(w, "This route could not be found")
-}
-
-func errorResponse(w http.ResponseWriter, message string) {
-	err := struct {
-		Success bool   `json:"success"`
-		Message string `json:"message"`
-	}{false, message}
-
-	json.NewEncoder(w).Encode(err)
-}
--- backend/server/router.go
+++ backend/server/router.go
@@ -9,13 +9,8 @@ import (
 func (s *Server) setupRoutes() http.Handler {
 	mux := mux.NewRouter()
 
-	// configure routes
 	mux.HandleFunc("/api/challanges", s.getChallangesHandler).Methods("GET")
 	mux.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
-	// custom 404 response
-	mux.NotFoundHandler = http.HandlerFunc(s.notFoundHandler)
-
-	// return CORS enabled http.Handler
 	return mux
 }
