0833f0bf6793fed080b83a77aecf95cda6a7890a
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2018-09-09 15:18:15 +0200

    Fade in user avatars

--- frontend/src/components/Option.js
+++ frontend/src/components/Option.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Image from "react-graceful-image";
 
 const Option = ({ avatar, name, onClick }) => (
   <li className="option" onClick={onClick}>
-    <img className="option--avatar" src={avatar} alt={`avatar of ${name}`} />
+    <Image
+      src={avatar}
+      alt={`avatar of ${name}`}
+      width={192}
+      height={192}
+      className="option--avatar"
+    />
     <p className="option--name">{name}</p>
   </li>
 );
