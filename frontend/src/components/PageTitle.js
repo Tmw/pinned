ac06967825d16890ff0e09ed388a18fd2ee51879
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2018-10-08 22:33:58 +0200

    Fix #21

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 
-const mainTitleDefault = "📌 Pinned! - ";
+const mainTitleDefault = "Pinned! - ";
 const makeTitle = ({ mainTitle = mainTitleDefault, subtitle }) =>
   mainTitle.concat(subtitle);
 
